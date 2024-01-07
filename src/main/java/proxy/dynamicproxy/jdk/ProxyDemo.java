e942f2f43fad219ed479761d6fe27286a31eb030
cliffswallow <cliffswallow@qq.com>
2024-01-08 00:36:48 +0800

    代理模式：JDK动态代理 添加说明

--- src/main/java/proxy/dynamicproxy/jdk/ProxyDemo.java
+++ src/main/java/proxy/dynamicproxy/jdk/ProxyDemo.java
@@ -2,7 +2,11 @@ package proxy.dynamicproxy.jdk;
 
 /**
  * @author Qiu Tao
- * @description: TODO
+ * @description: 被代理的类必须实现接口，才能基于 JDK Proxy API 实现动态代理，
+ * 当我们需要对二方或者三方提供的类实现代理的话，这将是个不小的限制。
+ * Proxy API 背后的原理是基于我们传入 interfaces，
+ * 动态创建了一个继承 Proxy 类并实现了我们提供的 interfaces 的类的字节码，
+ * 并通过我们传入的 classLoader 将这个类加载进来
  * @date 2024/1/8 0:03
  */
 public class ProxyDemo {
